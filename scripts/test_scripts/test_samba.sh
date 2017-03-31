aebbd3181a36d92c80c1e82eeaafe49730bcf653
Julien M'Poy <julien.mpoy@gmail.com>
2017-03-31 16:32:29 +0200

    Simple test to check if samba server is availbale.

--- scripts/test_scripts/test_samba.sh
+++ scripts/test_scripts/test_samba.sh
@@ -1,5 +1,33 @@
-#! /bin/bahs
+#! /bin/bash
 
-SMB_HOST=youSambaHostName
+USERNAME=$(whoami)
 
-smbclient -L SMB_HOST -U%
+
+echo -n "Enter your Samba server name or IP: "
+read SMB_HOST
+#echo -n "Enter your Samba directory name: "
+#read SMB_DIR
+#echo -n "Enter your Samba username: "
+#read SMB_USER
+#echo -n "Enter your Samba password: "
+#read -s SMB_PASSWORD
+
+smbclient -L $SMB_HOST -U%
+
+#SMB_TEST_DIR=test-samba
+#mkdir $SMB_TEST_DIR
+
+#echo "Mounting..."
+#echo
+
+#sudo mount -t cifs //$SMB_HOST/$SMB_DIR $SMB_TEST_DIR/ -o user=$USERNAME,password=$SMB_PASSWORD,workgroup=workgroup,uid=$(id -u $USERNAME),gid=$(id -g $USERNAME)
+
+#echo "Unmouting..."
+#echo
+
+#sudo umount $SMB_TEST_DIR || sudo fusermount -u $SMB_TEST_DIR
+#rm -r $SMB_TEST_DIR
+
+#echo "Done"
+#echo
+
